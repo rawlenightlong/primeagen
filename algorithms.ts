c087193e8c7815d85d7cf418652149f72cff4f76
Rawle Becerril <rawlesxm@gmail.com>
2024-06-11 23:07:36 -0400

    m

---
+++ algorithms.ts
@@ -0,0 +1,11 @@
+// our first search
+//
+
+function linearSearch(haystack: number[], needle: number): boolean {
+    for (let i = 0; i < haystack.length; i++) {
+        if (haystack[i] === needle) {
+            return true
+        }
+    }
+    return false
+}
